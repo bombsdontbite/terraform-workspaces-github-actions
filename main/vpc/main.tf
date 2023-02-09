50aa5e549bdccc97ea3b2ba50327a639ef2a0c7f
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 11:52:06 +0100

    Update main.tf

--- main/vpc/main.tf
+++ main/vpc/main.tf
@@ -1,5 +1,19 @@
 # LOCALS
 
 locals {
-  tags = merge(var.tags, { "Environment" = terraform.workspace })
+  tags = merge(var.tags, { "Project" = var.project, "Environment" = terraform.workspace })
+}
+
+# MODULES & RESOURCES
+
+module "vpc" {
+  source = "github.com/terraform-aws-modules/terraform-aws-vpc"
+  providers = {
+    aws = aws.target
+  }
+  name           = "${terraform.workspace}-${var.project}-vpc"
+  cidr           = var.vpc_cidr
+  azs            = try(var.vpc_azs, slice(data.aws_availability_zones.target.names, 0, 3))
+  public_subnets = var.vpc_public_subnets
+  tags           = local.tags
 }
