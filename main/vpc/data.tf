d0825063d851568fe36387152b625b627cb0dafa
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 19:41:06 +0100

    Update data.tf

--- main/vpc/data.tf
+++ main/vpc/data.tf
@@ -4,3 +4,11 @@ data "aws_availability_zones" "target" {
   provider = aws.target
   state    = "available"
 }
+
+data "aws_caller_identity" "target" {
+  provider = aws.target
+}
+
+data "aws_region" "target" {
+  provider = aws.target
+}
