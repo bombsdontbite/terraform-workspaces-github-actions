1a5095911e5b5424348a632189b9698d312d3594
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 13:33:36 +0100

    Update outputs.tf

--- main/vpc/outputs.tf
+++ main/vpc/outputs.tf
@@ -1,1 +1,17 @@
 # OUTPUTS
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+  
+output "azs" {
+  value = module.vpc.azs
+}
+  
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+  
+output "http_security_group_id" {
+  value = module.http_security_group.security_group_id
+}
