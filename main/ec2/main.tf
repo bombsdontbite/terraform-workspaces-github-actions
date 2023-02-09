ae32d6154f5e8f9e7b9ab6182e7fd0ec05cde877
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 23:07:59 +0100

    Update main.tf

--- main/ec2/main.tf
+++ main/ec2/main.tf
@@ -3,3 +3,21 @@
 locals {
   tags = merge(var.tags, { "Environment" = terraform.workspace })
 }
+
+# MODULES & RESOURCES
+
+module "alb" {
+  source = "github.com/terraform-aws-modules/terraform-aws-alb"
+  providers = {
+    aws = aws.target
+  }
+  # tbd
+}
+
+module "autoscaling" {
+  source = "github.com/terraform-aws-modules/terraform-aws-autoscaling"
+  providers = {
+    aws = aws.target
+  }
+  # tbd
+}
