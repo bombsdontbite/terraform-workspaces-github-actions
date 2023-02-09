3524d5275cb0febb5c36d07006211d9ef8fa6547
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 16:57:59 +0100

    Update backend.tf

--- main/vpc/backend.tf
+++ main/vpc/backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "multi-environment-infrastructure-provisioning-state-bucket"
     key            = "vpc/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "eu-north-1"
     dynamodb_table = "multi-environment-infrastructure-provisioning-locktable"
     encrypt        = true
   }
