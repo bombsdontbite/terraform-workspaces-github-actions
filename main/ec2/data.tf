09526fc13f4def28983dac499568813d484d4553
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 23:52:13 +0100

    Update data.tf

--- main/ec2/data.tf
+++ main/ec2/data.tf
@@ -1,1 +1,11 @@
 # DATA
+
+data "terraform_remote_state" "aws_network" {
+  backend   = "s3"
+  workspace = terraform.workspace
+  config = {
+    bucket = "multi-environment-infrastructure-provisioning-state-bucket"
+    key    = "vpc/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
