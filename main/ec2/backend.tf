46f7344e78ac5c5ce96f176b16dcb22a8e6ea120
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-09 11:33:24 +0100

    Files added

--- main/ec2/backend.tf
+++ main/ec2/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "multi-environment-infrastructure-provisioning-state-bucket"
-    key            = "terraform-stack-ec2/terraform.tfstate"
+    key            = "ec2/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "multi-environment-infrastructure-provisioning-locktable"
     encrypt        = true
--- main/vpc/backend.tf
+++ main/vpc/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "multi-environment-infrastructure-provisioning-state-bucket"
-    key            = "terraform-stack-network/terraform.tfstate"
+    key            = "vpc/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "multi-environment-infrastructure-provisioning-locktable"
     encrypt        = true
