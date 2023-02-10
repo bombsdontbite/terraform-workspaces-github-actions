94f53290923a6464ab551e06a57378a6b973cbcf
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-10 11:22:52 +0100

    Update outputs.tf

--- main/ec2/outputs.tf
+++ main/ec2/outputs.tf
@@ -1,6 +1,6 @@
 # OUTPUTS
 
 output "alb_dns_name" {
-  description = "The DNS name of the load balancer"
+  description = "The DNS name of the load balancer."
   value       = module.alb.lb_dns_name
 }
