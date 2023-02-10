5c9c284cc00e1482620a570f2092923c32f80266
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-10 09:47:09 +0100

    Update main.tf

--- main/ec2/main.tf
+++ main/ec2/main.tf
@@ -11,9 +11,27 @@ module "alb" {
   providers = {
     aws = aws.target
   }
-  name   = "${terraform.workspace}-${var.project}-lb"
-  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-  # tbd
+  name            = "${terraform.workspace}-${var.project}-lb"
+  internal        = var.alb_internal
+  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnets         = data.terraform_remote_state.vpc.outputs.public_subnets
+  security_groups = [data.terraform_remote_state.vpc.outputs.http_security_group_id]
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+  target_groups = [
+    {
+      name             = "${terraform.workspace}-${var.project}-tg"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+  tags = local.tags
 }
 
 module "autoscaling" {
