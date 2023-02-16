542383b8868dfe00cf90db9ce260ca9f990b9f49
Kirill Ushkalov <95179285+bombsdontbite@users.noreply.github.com>
2023-02-16 18:55:59 +0100

    Update variables.tf

--- main/ec2/variables.tf
+++ main/ec2/variables.tf
@@ -70,3 +70,99 @@ variable "alb_load_balancer_delete_timeout" {
   type        = string
   default     = "10m"
 }
+
+variable "autoscaling_key_name" {
+  description = "The key name that should be used for the instance."
+  type        = string
+  default     = null
+}
+
+variable "autoscaling_ignore_desired_capacity_changes" {
+  description = "Determines whether the `desired_capacity` value is ignored after initial apply."
+  type        = bool
+  default     = false
+}
+
+variable "autoscaling_min_size" {
+  description = "The minimum size of the autoscaling group."
+  type        = number
+  default     = null
+}
+
+variable "autoscaling_max_size" {
+  description = "The maximum size of the autoscaling group."
+  type        = number
+  default     = null
+}
+
+variable "autoscaling_desired_capacity" {
+  description = "The number of Amazon EC2 instances that should be running in the autoscaling group."
+  type        = number
+  default     = null
+}
+
+variable "autoscaling_wait_for_capacity_timeout" {
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out."
+  type        = string
+  default     = "10m"
+}
+
+variable "autoscaling_default_instance_warmup" {
+  description = "Amount of time, in seconds, until a newly launched instance can contribute to the Amazon CloudWatch metrics. This delay lets an instance finish initializing before Amazon EC2 Auto Scaling aggregates instance metrics, resulting in more reliable usage data. Set this value equal to the amount of time that it takes for resource consumption to become stable after an instance reaches the InService state."
+  type        = number
+  default     = null
+}
+
+variable "autoscaling_update_default_version" {
+  description = "Whether to update Default Version each update."
+  type        = bool
+  default     = true # The only available value within this project.
+}
+
+variable "autoscaling_instance_type" {
+  description = "The type of the instance."
+  type        = string
+  default     = null
+}
+
+variable "autoscaling_ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized."
+  type        = bool
+  default     = null
+}
+
+variable "autoscaling_enable_monitoring" {
+  description = "Enables/disables detailed monitoring."
+  type        = bool
+  default     = true
+}
+
+variable "autoscaling_create_iam_instance_profile" {
+  description = "Determines whether an IAM instance profile is created or to use an existing IAM instance profile."
+  type        = bool
+  default     = true # The only available value within this project.
+}
+
+variable "autoscaling_delete_on_termination" {
+  description = "Whether the volume should be destroyed on instance termination."
+  type        = bool
+  default     = true
+}
+
+variable "autoscaling_encrypted" {
+  description = "Enables EBS encryption on the volume."
+  type        = bool
+  default     = true
+}
+
+variable "autoscaling_volume_size" {
+  description = "The size of the volume in gigabytes."
+  type        = number
+  default     = null
+}
+
+variable "autoscaling_volume_type" {
+  description = "The volume type."
+  type        = string
+  default     = null
+}
